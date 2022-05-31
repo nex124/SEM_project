5840c8632ddc0b803fa739803a0706797a5c9f52
praavin0410 <67684016+praavin0410@users.noreply.github.com>
2022-06-01 02:25:11 +0800

    add button
    I had add a button for edit booking, and changed database name in the connection

--- ApplicationLayer/BookItem/ViewStatusBooking.php
+++ ApplicationLayer/BookItem/ViewStatusBooking.php
@@ -176,6 +176,7 @@ h2{font-family: Myriad Pro Light;}
 	<center>
 		<button class="btn active"><a href="http://localhost/IVMS/ApplicationLayer/BookItem/BookItem.php">View Item</button>
 		<button class="btn active"><a href="http://localhost/IVMS/ApplicationLayer/BookItem/AddBookForm.php">Add Booking</a></button>
+		<button class="btn active"><a href="http://localhost/IVMS/ApplicationLayer/BookItem/EditBookForm.php">Edit Booking</a></button>
     <button class="btn active"><a href="http://localhost/IVMS/ApplicationLayer/BookItem/ViewStatusBooking.php" target="_blank">View Booking </a></button>
   </center>
 		  <br><br>
@@ -197,11 +198,11 @@ h2{font-family: Myriad Pro Light;}
 			</tr>
 			<?php 
 
-$connection = mysqli_connect("localhost:3307","root","","test");
+$connection = mysqli_connect("localhost:3306","root","","mydatabase");
 
 if(!$connection)
 {
-	echo "Database connection faild...";
+	echo "Database connection failed...";
 }
 
 $retrive = mysqli_query($connection, "SELECT * FROM booking ");
